d2186a4f909fe3b2da1b5d6dd43a44bf80aec191
23Vatsal <vatsalbhatnagar007@gmail.com>
2020-06-30 11:00:44 +0530

    used arrow function

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
-function greet(){
-    return <h1>Hello Vatsal!</h1>
-}
+// function greet(){
+//     return <h1>Hello Vatsal!</h1>
+// }
+
+const greet=()=><h1>Hello Vatsal!</h1>
 
 export default greet
