2be9f12a89a6ef8748b8b4b0d6562c082206f6da
23Vatsal <vatsalbhatnagar007@gmail.com>
2020-07-01 10:41:05 +0530

    without JSX

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 const Hello = () => {
-    return (
-        <div>
-            <h3>Hello JSX</h3>
-        </div>
-    )
+    // return (
+    //     <div>
+    //         <h3>Hello JSX</h3>
+    //     </div>
+    // )
+
+    return React.createElement('div',{id: 'hello' , className: 'dummyClass'},
+    React.createElement('h1',null,'Hello without JSX'))
 }
 
 export default Hello
