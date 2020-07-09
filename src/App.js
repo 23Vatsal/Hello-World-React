3b656473bb6b282ce0a9b6c9806279bda14751fd
23Vatsal <vatsalbhatnagar007@gmail.com>
2020-07-09 18:22:54 +0530

    Inline components

--- src/App.js
+++ src/App.js
@@ -13,11 +13,17 @@ import ParentComponent from './components/ParentComponent';
 import WelcomeGreeting from './components/WelcomeGreeting';
 import NameList from './components/NameList';
 import StyleSheet from './components/StyleSheet'
+import Inline from './components/Inline';
+import './appStyles.css'
+import styles from './appStyles.module.css'
 
 function App() {
   return (
     <div className="App">
-      <StyleSheet primary={true}/>
+      <Inline />
+      <h1 className='error'>Error</h1>
+      <h1 className={styles.success}>Success</h1>
+      {/* <StyleSheet primary={true}/> */}
       {/* <NameList/> */}
       {/* <WelcomeGreeting/> */}
       {/* <ParentComponent/> */}
