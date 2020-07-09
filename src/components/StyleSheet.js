3868fd21f6b84bdacec3a33b18c0131aa47e384c
23Vatsal <vatsalbhatnagar007@gmail.com>
2020-07-09 18:05:07 +0530

    CSS(Style Sheet)

--- src/components/StyleSheet.js
+++ src/components/StyleSheet.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './myStyles.css'
 
-function StyleSheet() {
+function StyleSheet(props) {
+    const className= props.primary ? 'primary' : ''
     return (
         <div>
-            <h2 className='primary'>Style Sheet</h2>
+            <h2 className={`${className} font-xl` }>Style Sheet</h2>
         </div>
     )
 }
